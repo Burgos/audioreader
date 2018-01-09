82a35ea62e62034965d088358e6b6096e242138a
Nemanja Boric <4burgos@gmail.com>
2018-01-09 22:49:12 +0100

    Add iterator over shared_ptr<WavFile>
    This adds the iteration support (and begin/end functions) for
    shared_ptr<WavFile>. Iterator iterates over the samples, and each sample
    provides operator[] returning the channel and each channel provides the
    operator[] over the data.
    
    Note that instead of the exceptions, all errors in handling this are
    asserted at the moment. This should probably be revisited, if we are
    going to allow loading non-trusted wav files.

--- WavReader/WavReader.h
+++ WavReader/WavReader.h
@@ -3,6 +3,7 @@
 #include <array>
 #include <vector>
 #include <cstddef>
+#include <memory>
 
 struct WavFile {
     /// Chunk id, RIFF, big endian
@@ -61,11 +62,122 @@ struct WavFile {
     WavFile(const WavFile& wf) = default;
     WavFile& operator=(const WavFile& wf) = default;
 
+    bool operator!=(const WavFile& other) const
+    {
+        return this->datachk_size != other.datachk_size ||
+            this->data != other.data;
+               
+    }
+
+
 private:
     WavFile() {}
 
     friend WavFile generateWavFromData(std::vector<std::byte> data);
 };
+
+
+// Sample
+struct Sample
+{
+    struct Channel
+    {
+        std::byte operator[](std::size_t idx) const
+        {
+            auto bytes_per_sample{ (*s.file).fmtchk_bitspersample / 8 };
+            assert(idx >= 0 && static_cast<int>(idx) < bytes_per_sample);
+
+            return (*s.file).data_it[s.sample_offset + ch * bytes_per_sample + idx];
+        }
+
+    private:
+        const Sample& s;
+        int ch;
+        Channel(const Sample& sample, int channel):s{ sample }, ch{ channel } {}
+        friend struct Sample;
+    };
+
+    Channel operator[](int channel) const
+    {
+        assert(channel < number_of_channels());
+        return Channel(*this, channel);
+    }
+
+    int number_of_channels() const { return (*file).fmtchk_numchannels; }
+    int channel_size() const { return (*file).fmtchk_bitspersample; }
+private:
+    // file
+    std::shared_ptr<WavFile> file;
+
+    // sample offset
+    int32_t sample_offset;
+
+    int16_t bits_per_sample;
+
+    Sample(std::shared_ptr<WavFile> f, int32_t off) : file(f), sample_offset(off)
+    {
+        bits_per_sample = (*f).fmtchk_bitspersample;
+    }
+
+    friend class iterator;
+};
+
+// sample iterator
+class iterator {
+public:
+    iterator& operator+=(int samples)
+    {
+        this->offset += samples * ((*file).fmtchk_numchannels * (*file).fmtchk_bitspersample / 8);
+        assert(this->offset >= 0);
+        assert(this->offset <= (*file).datachk_size);
+        return *this;
+    }
+
+    iterator& operator-=(int samples)
+    {
+        return operator+=(-samples);
+    }
+
+    iterator& operator++()
+    {
+        return operator+=(1);
+    }
+
+    iterator& operator--()
+    {
+        return operator+=(-1);
+    }
+
+    bool operator!= (const iterator& rhs) const
+    {
+        return this->offset != rhs.offset ||
+            this->file != rhs.file;
+    }
+
+    Sample operator*() const
+    {
+        Sample s{ this->file, this->offset };
+        return s;
+    }
+
+private:
+    iterator(std::shared_ptr<WavFile> wav, int32_t off) : file{ wav }, offset{ off } {}
+    std::shared_ptr<WavFile> file;
+    int32_t offset;
+
+    friend iterator begin(std::shared_ptr<WavFile>);
+    friend iterator end(std::shared_ptr<WavFile>);
+};
+
+iterator begin(std::shared_ptr<WavFile> file)
+{
+    return iterator{ file, 0 };
+}
+
+iterator end(std::shared_ptr<WavFile> file)
+{
+    return iterator{ file, (*file).datachk_size };
+}
 
 /// Generate the WavFile from data.
 WavFile generateWavFromData(std::vector<std::byte> data);
